27308973c4f0cd7452c075a61110e7824385e902
Daniel <zodexnl@gmail.com>
2022-04-25 17:26:52 +0200

    .gitignore and api keys added

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import requests
+import youtube_dl
+from googleapiclient.discovery import build
+
+# Import this for the API keys
+import apikeys
+
+playlistId = input("Playlist ID")
+
+
+
