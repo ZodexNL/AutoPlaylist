99872d5a2578b49d798d8a0ba1c6fba2aa27e365
Daniel <zodexnl@gmail.com>
2022-05-02 22:01:15 +0200

    Added the project plan in comments main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+
+First we have to make sure we are logged into YouTube
+Then we have to somehow grab our video's (maybe via playlist)(or via the songs that I liked on YouTube
+The next step is to create a new playlist in spotify
+And then we need to make sure that we search the song on Spotify
+Lastly we need to add this song into our new Spotify playlist
+
+"""
