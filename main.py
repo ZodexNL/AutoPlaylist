839037f26311620191f018d87ce81ee5e98b8bff
Daniel <zodexnl@gmail.com>
2022-05-02 22:25:38 +0200

    created functions

--- main.py
+++ main.py
@@ -10,3 +10,31 @@ Lastly we need to add this song into our new Spotify playlist
 
 
 
+class CreatePlaylist:
+
+    # This method is called everytime an object is created from a class
+    # __init__ initializes the object's attributes
+    def __init__(self):
+        # pass means that is passes the method
+        pass
+
+    # Function to log into YouTube
+    def get_youtube_client(self):
+        pass
+
+    # Function to get our liked videos
+    def get_liked_videos(self):
+        pass
+
+    # Function that creates a new spotify playlist
+    def create_playlist(self):
+        pass
+
+    # Function that searches the song on spotify
+    def get_spotify_uri(self):
+        pass
+
+    # Function to add the song to the spotify playlist
+    def add_song_to_playlist(self):
+        pass
+
