f40176a5be23c4f1a6b00d97f918b63525efc2df
Daniel <zodexnl@gmail.com>
2022-05-06 23:27:05 +0200

    Working version, this now creates a spotify playlist

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-# Start of the app
+# imports
+import requests
+
+from apikeys import spotify_user_id, spotify_token
+
+
+def create_playlist(name, public):
+    response = requests.post(
+        spotify_user_id,
+        headers={
+            "Authorization": f"Bearer {spotify_token}"
+        },
+        json={
+            "name": name,
+            "public": public
+        }
+    )
+    json_resp = response.json()
+
+    return json_resp
+
+
+def main():
+    playlist = create_playlist(
+        name="Mijn Playlist",
+        public=False
+    )
+
+    print(f"Playlist: {playlist}")
+
+
+if __name__ == '__main__':
+    main()
